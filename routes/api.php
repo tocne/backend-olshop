7a416b815dcee573db0f7dd0173c4d6f1b29d4ae
tocne <hafidz.hamda@gmail.com>
2025-11-29 15:30:12 +0700

    fix cors preflight

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,13 @@ use App\Http\Controllers\Api\SeriesController;
 use App\Http\Controllers\Api\UploadController;
 use Illuminate\Support\Facades\Route;
 
+Route::options('/{any}', function () {
+    return response('')
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+})->where('any', '.*');
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 // Tes endpoint bawaan
