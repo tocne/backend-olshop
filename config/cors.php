2aa4a2df65ecaa116713ff84a047181b4ff6f231
tocne <hafidz.hamda@gmail.com>
2025-12-21 13:58:29 +0700

    DOMAIN

--- config/cors.php
+++ config/cors.php
@@ -10,6 +10,7 @@ return [
         'http://localhost:5173',
         'http://localhost:3000',
         'https://teekay.onrender.com',
+        'https://teekay-pi.vercel.app',
         '*'
      ],
 
